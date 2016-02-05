336c1795dd86deb8ee63ca00f5872f630bbc908d
natascha <natascha.abraschitowa@gmail.com>
2016-02-05 15:57:56 +0300

    changes

--- parallel_course/redirect/main.cpp
+++ parallel_course/redirect/main.cpp
@@ -7,18 +7,34 @@
 #include <stdlib.h> 
 #include <fcntl.h>
                                                                                 
-int main(){                                                                     
+
+void command_arg_parser(std::string & str, std::string &  command, std::string &  arg){
+    
+ std::size_t pos = str.find(" ");
+ command = str.substr(0,pos);
+     if (pos != std::string::npos){
+         arg=str.substr(pos);
+     }   
+    
+    
+}
+
+int main(){ 
+	remove("/home/box/result.out");                                                                    
 	std::string temp;                                                         
-	std::vector<std::string> vect;                                                  
+	std::vector<std::string> vect;
+	std::string string_arg; 
 	std::getline(std::cin,temp);                                                    
-	std::stringstream strm(temp);                                                   
-                                                                                
-                                                                                
-	while (std::getline(strm,temp,'|')){                                           
-		vect.push_back(temp);                                                          
-	}                                                                               
-                                                                                
+	std::stringstream strm(temp);  
 	int pfd[2];
+                                                                                
+                                                                                
+        	while (std::getline(strm,temp,'|')){                                           
+        		vect.push_back(temp);                                                          
+        	} 
+        	
+    
+     
     	
 for (int i =0; i< vect.size()-1; i++){
     pipe(pfd);
@@ -30,7 +46,13 @@ for (int i =0; i< vect.size()-1; i++){
 		dup2(pfd[0],STDIN_FILENO); //redirect;
 		close(pfd[1]); //old fd
 		close(pfd[0]); // close old fd
-		execlp(vect[i].c_str(),vect[i].c_str(),NULL); 
+		std::string command;
+		std::string arg;
+		command_arg_parser(vect[i], command, arg);
+		if (arg.size()==0)
+		    execlp(command.c_str(),command.c_str(),NULL);
+		else
+		    execlp(command.c_str(),command.c_str(),arg.c_str());
 	}
 }
 
@@ -38,10 +60,16 @@ pipe(pfd);
 close(STDIN_FILENO); //close stdin
 dup2(pfd[0],STDIN_FILENO); //redirect;
 close(pfd[0]); // close old fd
-int f = open("result.out",O_RDWR,0666);
+int f = open("/home/box/result.out",O_CREAT|O_RDWR,0666);
 dup2(f,STDOUT_FILENO);
 close(f); // close old fd
-execlp(vect[vect.size()-1].c_str(),vect[vect.size()-1].c_str(),NULL);
+		std::string command;
+		std::string arg;
+		command_arg_parser(vect[vect.size()-1], command, arg);
+		if (arg.size()==0)
+		    execlp(command.c_str(),command.c_str(),NULL);
+		else
+		    execlp(command.c_str(),command.c_str(),arg.c_str());
 /*for (int i =0; i< vect.size(); i++){                                             
 	pipe(fd);
 	close(stdin);

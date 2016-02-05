90c9282e1e08d462344646765c4a9de456464e03
natascha <natascha.abraschitowa@gmail.com>
2016-02-05 14:30:12 +0300

    added first version of redirect program with pipes

---
+++ parallel_course/redirect/main.cpp
@@ -0,0 +1,55 @@
+#include <stdio.h> 
+#include <unistd.h>                                                             
+#include <iostream>                                                             
+#include <string>                                                               
+#include <vector>                                                               
+#include <sstream> 
+#include <stdlib.h>  
+                                                                                
+int main(){                                                                     
+	std::string temp;                                                         
+	std::vector<std::string> vect;                                                  
+	std::getline(std::cin,temp);                                                    
+	std::stringstream strm(temp);                                                   
+                                                                                
+                                                                                
+	while (std::getline(strm,temp,'|')){                                           
+		vect.push_back(temp);                                                          
+	}                                                                               
+                                                                                
+	int pfd[2];
+    	
+for (int i =0; i< vect.size()-1; i++){
+    pipe(pfd);
+	if (!fork()){ //child
+	
+		close (STDOUT_FILENO); //close stdout
+		close(STDIN_FILENO); //close stdin
+		dup2(pfd[1],STDOUT_FILENO); //redirect
+		dup2(pfd[0],STDIN_FILENO); //redirect;
+		close(pfd[1]); //old fd
+		close(pfd[0]); // close old fd
+		execlp(vect[i].c_str(),vect[i].c_str(),NULL); 
+	}
+}
+
+pipe(pfd);
+close(STDIN_FILENO); //close stdin
+dup2(pfd[0],STDIN_FILENO); //redirect;
+close(pfd[0]); // close old fd
+execlp(vect[vect.size()-1].c_str(),vect[vect.size()-1].c_str(),NULL); 
+/*for (int i =0; i< vect.size(); i++){                                             
+	pipe(fd);
+	close(stdin);
+	dub3(fd[1],0,O_CLOEXEC); // 0 - stdin
+	pid_t pid = fork();
+	if (pid){  //child                                                          
+     close(fd[1]); // close output;
+	 std::cin>>fd[0];
+	 exec(vect[i].c_str());                                                                           
+	}else { //parent
+	close(fd[0]); //close input
+	
+	}
+*/
+}

74cb040fc61f8483ed196647829e28a71f18b747
Tasha <natascha@rosta.ru>
2016-02-08 01:39:40 +0300

    named_channels

---
+++ parallel_course/named_channels/main.c
@@ -0,0 +1,24 @@
+#include <stdio.h> 
+#include <unistd.h>
+#include <stdlib.h> 
+#include <fcntl.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <limits.h>
+
+
+int main(){
+char * path_i = "/home/box/in.fifo";
+char * path_o = "/home/box/out.fifo";
+mkfifo(path_i, 0666);
+mkfifo(path_o, 0666);
+int f_i = open(path_i,O_CREAT|O_RDONLY);
+int f_o = open(path_i,O_CREAT|O_WRONLY);
+char temp[PIPE_BUF];
+while(1){
+	read(f_i, temp, PIPE_BUF);
+	write(f_o, temp, PIPE_BUF);
+}
+
+
+}

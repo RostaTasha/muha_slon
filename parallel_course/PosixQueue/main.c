06fbe5a18f3aa591ff97bcd9fafc69265f81fce7
Tasha <natascha@rosta.ru>
2016-02-10 10:24:18 +0300

    added attr

--- parallel_course/PosixQueue/main.c
+++ parallel_course/PosixQueue/main.c
@@ -2,12 +2,16 @@
 #include <fcntl.h>
 #include <sys/stat.h>
 #include <mqueue.h>
-
+#define SIZE 100
 int main(){
-
-mqd_t mqdes =  mq_open("/test.mq",O_CREAT | O_RDWR,0666,NULL);
-char buf[9];
-int msg_size = mq_receive( mqdes, buf, 9, NULL );
+struct mq_attr attr;
+attr.mq_flags = 0;
+attr.mq_maxmsg = 10;
+attr.mq_msgsize = SIZE;
+attr.mq_curmsgs = 0;
+mqd_t mqdes =  mq_open("/test.mq",O_CREAT | O_RDWR,0666,&attr);
+char buf[SIZE+1];
+int msg_size = mq_receive( mqdes, buf, 100, NULL );
 buf[msg_size]='\0';
 FILE * f = fopen("/home/box/message.txt","w");
 fwrite(buf,1,msg_size, f);

b254c6d036a03d0f809843ef2009df7ca126d503
RostaTasha <natascha@rosta.ru>
2016-02-10 07:15:16 +0000

    small changes

--- parallel_course/PosixQueue/main.c
+++ parallel_course/PosixQueue/main.c
@@ -5,13 +5,15 @@
 
 int main(){
 
-mqd_t mqdes =  mq_open("/test.mq",O_CREAT | O_RDONLY);
-char buf[100];
-int msg_size = mq_receive( mqdes, buf, 100, NULL );
+mqd_t mqdes =  mq_open("/test.mq",O_CREAT | O_RDWR,0666,NULL);
+char buf[9];
+int msg_size = mq_receive( mqdes, buf, 9, NULL );
+buf[msg_size]='\0';
 FILE * f = fopen("/home/box/message.txt","w");
 fwrite(buf,1,msg_size, f);
 fclose(f);
-
+mq_close(mqdes);
+mq_unlink("/test.mq");
 
 
 }

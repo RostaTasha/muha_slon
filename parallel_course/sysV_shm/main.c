3d1f3ce2ed2a1956baa5ded47e985a36f00499fe
Tasha <natascha@rosta.ru>
2016-02-11 02:21:02 +0300

    fixed errors

--- parallel_course/sysV_shm/main.c
+++ parallel_course/sysV_shm/main.c
@@ -1,15 +1,24 @@
-#include <stdio.h>
-#include <sys/types.h>
-#include <sys/shm.h>
-
-int main(){
-key_t key = ftok("/tmp/mem.temp", 1);
-int shmid = shmget(key,2*1024*1024,IPC_CREAT);
-void  * start_addr  = shmat(shmid,NULL,0);
-int i =0;
-for (i=0; i<1024*1024;i++){
-	*((char *)(start_addr)+i)=42;
-}
-return 0;
-
-}
+#include <stdio.h>                                                              
+#include <sys/types.h>                                                          
+#include <sys/shm.h>                                                            
+#include <stdlib.h>                                                             
+                                                                                
+int main(){                                                                     
+key_t key = ftok("/tmp/mem.temp", 1);                                           
+unsigned int size = 2*1024*1024; 
+unsigned int sizem  = 1024*1024;                                               
+int shmid = shmget(key,size,0666|IPC_CREAT);                                    
+printf("\nshmid %d\n",shmid);
+if (shmid==-1) {
+printf("Error!!!\n");
+return -1;
+}                                                   
+void  * start_addr  = shmat(shmid,NULL,0);                                      
+int i =0;                                                                       
+for (i=0; i<sizem;i++){                                                     
+        *((char *)(start_addr)+i)=42;                                           
+}                                                                               
+return 0;                                                                       
+                                                                                
+}                                                                               
+       

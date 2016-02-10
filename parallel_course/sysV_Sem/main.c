0da3eb4d25ffa374e7ee3520201839b0f1ccb011
RostaTasha <natascha@rosta.ru>
2016-02-10 19:04:47 +0000

    test works

--- parallel_course/sysV_Sem/main.c
+++ parallel_course/sysV_Sem/main.c
@@ -9,12 +9,20 @@ key_t  key = ftok("/tmp/sem.temp", 1);
 //int semid =  semget(key_t key, int nsems, int semflg); 
 int semid = semget(key, 16, 0666|IPC_CREAT);
 int i;
-
+union semun {
+int val;
+struct semid_ds *buf;
+unsigned short * array;
+struct seminfo *__buf;
+};
+union semun arg;
 struct sembuf op[16];
 for (i =0; i<16; i++){
-op[i].sem_num = i; //signifies 0th semaphore
-op[i].sem_op = 1; //reduce the semaphore count to lock
-op[i].sem_flg = 0; //wait till we get lock on semaphore
+arg.val=i;
+//op[i].sem_num = i; //signifies 0th semaphore
+//op[i].sem_op = 1; //reduce the semaphore count to lock
+//op[i].sem_flg = 0; //wait till we get lock on semaphore
+semctl(semid,i,SETVAL,arg);
 }
-semop(semid, op, 16);
+//semop(semid, op, 16);
 }

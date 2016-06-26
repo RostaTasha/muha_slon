aeb33ab3758be7f3962da2c2619de879262d3603
RostaTasha <natascha@rosta.ru>
2016-06-26 15:58:41 +0300

    Update sol.cpp

--- ya/task2_3/sol.cpp
+++ ya/task2_3/sol.cpp
@@ -1,15 +1,14 @@
 #include<vector>
 
-double sum4(std::vector<double> &v){
-    double sum = 0;
+double sum4(std::vector<double> &v){double sum = 0;
     double c = 0;
-	int size = v.size();
+    int size = v.size();
     for (int i =0; i < size; i++) {
-		double y = v[i] - c;
-		double t = sum + y;
-		c = (t - sum) -y;
-		sum = t;
-	}
-	return sum;
-	}
+        double y = v[i] - c;
+        double t = sum + y;
+        c = (t - sum) -y;
+        sum = t;
+    }
+    return sum;
+}
 
